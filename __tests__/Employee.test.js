4cf00cc8c44da5b0b56d4320fdab01a74f57c08d
you know who <chorvickprocode@gmail.com>
2021-03-02 04:49:43 -0500

    test run now in npm but fail when should pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,6 +3,14 @@ const Employee = require("../lib/Employee");
 
 test("Should return an object.", () => {
     const Henry = new Employee()
-    expect(typeof (Henry)).tobe("object");
+    expect(typeof (Henry)).toBe("object");
+
+});
+
+test("Check to see if name can be set in class", () => {
+    const name = "Henry"
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name)
+
 
 });
