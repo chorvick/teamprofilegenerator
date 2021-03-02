fde71f7939fab98b2e735fc1e4c9c0a6d5667c38
you know who <chorvickprocode@gmail.com>
2021-03-02 14:10:21 -0500

    added manager tests- all tests pass now

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,16 @@
 /// manager test
 const Manager = require("../lib/Manager");
 
+test("Make sure office number works", () => {
+    const officeNum = 123;
+    const manager = new Manager("Henry", 999, "henry@henry.com", officeNum);
+    expect(manager.officeNum).toBe(officeNum);
+
+});
+
+
+test("Make sure getRole function returns Manager", () => {
+    const role = "Manager";
+    const manager = new Manager("Henry", 999, "Henry@Henry.com", 123);
+    expect(manager.getRole()).toBe(role);
+});
