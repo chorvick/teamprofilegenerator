451ca2d2b5dbb2aa51b1ae0499079da96533fbd0
you know who <chorvickprocode@gmail.com>
2021-03-02 07:23:20 -0500

    intern tests complete - all three pass

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,4 +2,28 @@
 const Intern = require("../lib/Intern");
 
 
+test("Make sure school works", () => {
+    const school = "xxx";
+    const intern = new Intern("Henry", 999, "henry@henry.com", school);
+    expect(intern.school).toBe(school);
+
+});
+
+
+test("Test interns getSchool function works properly", () => {
+    const school = "xxx";
+    const intern = new Intern("Henry", 999, "Henry@Henry.com", school);
+    expect(intern.getSchool()).toBe("xxx");
+
+
+});
+
+test("Make sure getRole function returns Intern", () => {
+    const role = "Intern";
+    const intern = new Intern("Henry", 999, "Henry@Henry.com", "xxx");
+    expect(intern.getRole()).toBe(role);
+});
+
+
+
 
