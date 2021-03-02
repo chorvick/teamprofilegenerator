660bdbb9307fedc44e8c221598d66671f96a32c6
you know who <chorvickprocode@gmail.com>
2021-03-02 04:33:13 -0500

    added one test - not able to run yet

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,8 @@
 //employee test
 const Employee = require("../lib/Employee");
 
+test("Should return an object.", () => {
+    const Henry = new Employee()
+    expect(typeof (Henry)).tobe("object");
+
+});
