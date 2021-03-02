14d39eb33dc65506f5306e911117007ea3b860b0
you know who <chorvickprocode@gmail.com>
2021-03-02 05:19:58 -0500

    corrected syntax in employee js file- tests now pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,15 +2,30 @@
 const Employee = require("../lib/Employee");
 
 test("Should return an object.", () => {
-    const Henry = new Employee()
+    const Henry = new Employee();
     expect(typeof (Henry)).toBe("object");
 
-});
-
+})
 test("Check to see if name can be set in class", () => {
-    const name = "Henry"
+    const name = "Henry";
     const employee = new Employee(name);
-    expect(employee.name).toBe(name)
+    expect(employee.name).toBe(name);
 
 
-});
+})
+
+test("check that id will return properly", () => {
+    const id = 999;
+    const employee = new Employee("Henry", id);
+    expect(employee.id).toBe(id);
+
+
+})
+
+test("check that e mail returns properly", () => {
+    const email = "Henry@Henry.com";
+    const employee = new Employee("Henry", 999, email);
+    expect(employee.email).toBe(email);
+
+
+})
