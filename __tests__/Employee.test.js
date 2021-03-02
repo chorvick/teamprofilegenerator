90d3204ac557d7b6c0425245c6af88d711c088c4
you know who <chorvickprocode@gmail.com>
2021-03-02 06:02:34 -0500

    finished employee tests - they all pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,14 +5,14 @@ test("Should return an object.", () => {
     const Henry = new Employee();
     expect(typeof (Henry)).toBe("object");
 
-})
+});
 test("Check to see if name can be set in class", () => {
     const name = "Henry";
     const employee = new Employee(name);
     expect(employee.name).toBe(name);
 
 
-})
+});
 
 test("check that id will return properly", () => {
     const id = 999;
@@ -20,12 +20,34 @@ test("check that id will return properly", () => {
     expect(employee.id).toBe(id);
 
 
-})
+});
 
 test("check that e mail returns properly", () => {
-    const email = "Henry@Henry.com";
+    const email = "Henry@Henry.com"
     const employee = new Employee("Henry", 999, email);
     expect(employee.email).toBe(email);
 
+
+});
+
+test("Make sure get name functions correctly", () => {
+    const name = "Henry"
+    const employee = new Employee(name)
+    expect(employee.getName()).toBe(name)
+
+
+});
+
+test("Make sure id is returned properly", () => {
+    const id = 999
+    const employee = new Employee("Henry", 999)
+    expect(employee.getId()).toBe(id)
+
+});
+
+test("Make sure email is returned properly", () => {
+    const email = "Henry@Henry.com"
+    const employee = new Employee("Henry", 999, email)
+    expect(employee.getEmail()).toBe(email)
 
 })
