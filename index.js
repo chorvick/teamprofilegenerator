874d338169bb45acd8fd414316f9c2a3d36538de
you know who <chorvickprocode@gmail.com>
2021-03-03 03:54:25 -0500

    fixed bug chrome was loading slow -icons now display

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const fs = require("fs");
-
+console.log("Team Profile Generator")
 const promptUser = (current_roster) => {
     let roster = current_roster || []
-    console.log("Team Profile Generator")
+    // console.log("Team Profile Generator")
     /// console.log(`Current roster: ${JSON.stringify(roster)}`)
     inquirer.prompt([
         {
@@ -78,7 +78,7 @@ function populate_cards(roster) {
         if (member.role == "Engineer") {
             // Make a card for an engineer
             out_cards += `<div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="..." alt="">
+            <img class="card-img-top" src="" >
             <div class="card-body">
             <span class="fas fa-glasses"></span>
               <h5 class="card-title">${member.name}</h5>
@@ -94,7 +94,7 @@ function populate_cards(roster) {
         if (member.role == "Intern") {
             // Make a card for an intern
             out_cards += `<div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="" alt="">
+            <img class="card-img-top" src="" >
             <div class="card-body">
             <span class="fas fa-glasses"></span>
               <h5 class="card-title">${member.name}</h5>
@@ -140,10 +140,11 @@ function render_team(team) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   <title>Team Profile</title>
 </head>
 <body>
@@ -161,9 +162,7 @@ ${populate_cards(team)}
 </div>
 </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </html>`
 
     return html
